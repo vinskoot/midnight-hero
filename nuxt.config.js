b6e3ad02de9de63719d51b11244e62c7db4c08fe
Vincent COUTY <vinskoot@gmail.com>
2019-09-24 17:13:23 +0200

    Fix deploiement config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,14 @@
+const routerBase =
+    process.env.DEPLOY_ENV === 'GH_PAGES'
+        ? {
+              router: {
+                  base: '/midnight-hero/'
+              }
+          }
+        : {};
+
 export default {
+    ...routerBase,
     mode: 'universal',
     /*
      ** Headers of the page
